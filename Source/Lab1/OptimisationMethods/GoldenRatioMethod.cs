dbac5d2cd24c41e176d46e45f154f0eb087a03d1
Bibletoon <alex8.ivanov@gmail.com>
2022-03-22 18:43:51 +0300

    Feat: add golden ratio method

---
+++ Source/Lab1/OptimisationMethods/GoldenRatioMethod.cs
@@ -0,0 +1,21 @@
+﻿namespace Lab1.OptimisationMethods;
+
+public class GoldenRatioMethod : IOptimisationMethod
+{
+    private static readonly decimal GoldenRatioProportion = (decimal)((1+Math.Sqrt(5))/2);
+
+    public (decimal, decimal) FindNewInterval(decimal a, decimal b, Func<decimal, decimal> function)
+    {
+        var x1 = b - (b - a) / GoldenRatioProportion;
+        var x2 = a + (b - a) / GoldenRatioProportion;
+
+        if (function.Invoke(x1) >= function.Invoke(x2))
+        {
+            return (x1, b);
+        }
+        else
+        {
+            return (a, x2);
+        }
+    }
+}
