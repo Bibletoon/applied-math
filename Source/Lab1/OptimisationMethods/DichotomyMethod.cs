9d90283d1256a9595e417f095ce1152ed2747e54
Bibletoon <alex8.ivanov@gmail.com>
2022-03-22 11:57:14 +0300

    Feat: add dichotomy method

---
+++ Source/Lab1/OptimisationMethods/DichotomyMethod.cs
@@ -0,0 +1,28 @@
+﻿namespace Lab1.OptimisationMethods;
+
+public class DichotomyMethod : IOptimisationMethodLogic
+{
+    private decimal _delta;
+
+    public DichotomyMethod(decimal delta)
+    {
+        _delta = delta;
+    }
+
+    public (decimal, decimal) FindNewInterval(decimal a, decimal b, Func<decimal, decimal> function)
+    {
+        var x1 = (a + b) / 2 - _delta;
+        var x2 = (a + b) / 2 + _delta;
+
+        if (function.Invoke(x1) < function.Invoke(x2))
+        {
+            return (a, x2);
+        }
+        if (function.Invoke(x2) < function.Invoke(x1))
+        {
+            return (x1, b);
+        }
+
+        return (x1, x2);
+    }
+}
