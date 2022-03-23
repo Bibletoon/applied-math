14ded7d6999a15c1c70bac53efb50e032f47fc16
Bibletoon <alex8.ivanov@gmail.com>
2022-03-23 15:05:21 +0300

    Feat: add parabola method

---
+++ Source/Lab1/OptimisationMethods/ParabolaMethod.cs
@@ -0,0 +1,39 @@
+﻿namespace Lab1.OptimisationMethods;
+
+public class ParabolaMethod : IOptimisationMethod
+{
+    private Random _random = new Random();
+    
+    public (decimal, decimal) FindNewInterval(decimal a, decimal b, Func<decimal, decimal> function)
+    {
+        decimal c = (decimal)_random.NextDouble() * (b - a) + a;
+
+        decimal u = c - (((c - a) * (c - a)) * (function.Invoke(c) - function.Invoke(b)) -
+                         ((c - b) * (c - b)) * (function.Invoke(c) - function.Invoke(a))) /
+            (2 * ((c - a) * (function.Invoke(c) - function.Invoke(b)) -
+                  (c - b) * (function.Invoke(c) - function.Invoke(a))));
+
+        if (c < u)
+        {
+            if (function.Invoke(c) < function.Invoke(u))
+            {
+                return (a, u);
+            }
+            else
+            {
+                return (c, b);
+            }
+        }
+        else
+        {
+            if (function.Invoke(c) < function.Invoke(u))
+            {
+                return (u, b);
+            }
+            else
+            {
+                return (a, c);
+            }
+        }
+    }
+}
