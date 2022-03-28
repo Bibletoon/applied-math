d469e4fcc3c05a8c9e92f27515713f45606a2cda
Bibletoon <alex8.ivanov@gmail.com>
2022-03-28 13:53:43 +0300

    Fix: decrease function calls count in golden ratio method

--- Source/Lab1/OptimisationMethods/GoldenRatioMethod.cs
+++ Source/Lab1/OptimisationMethods/GoldenRatioMethod.cs
@@ -2,23 +2,23 @@
 
 namespace Lab1.OptimisationMethods;
 
-public class GoldenRatioMethod : IOptimisationMethod<BoundedOptimizationContext>
+public class GoldenRatioMethod : IOptimisationMethod<GoldenRatioOptimisationContext>
 {
     private static readonly double GoldenRatioProportion = (1 + Math.Sqrt(5)) / 2;
 
-    public BoundedOptimizationContext FindNewInterval(BoundedOptimizationContext context, Func<double, double> function)
+    public GoldenRatioOptimisationContext FindNewInterval(GoldenRatioOptimisationContext context, Func<double, double> function)
     {
         var (a, b) = (context.A, context.B);
-        var x1 = b - (b - a) / GoldenRatioProportion;
-        var x2 = a + (b - a) / GoldenRatioProportion;
+        var x1 = context.X1 ?? b - (b - a) / GoldenRatioProportion;
+        var x2 = context.X2 ?? a + (b - a) / GoldenRatioProportion;
 
         if (function.Invoke(x1) >= function.Invoke(x2))
         {
-            return new BoundedOptimizationContext(x1, b);
+            return new GoldenRatioOptimisationContext(x1, b, x2, null);
         }
         else
         {
-            return new BoundedOptimizationContext(a, x2);
+            return new GoldenRatioOptimisationContext(a, x2, null, x1);
         }
     }
 }
---
+++ Source/Lab1/OptimizationContexts/GoldenRatioOptimisationContext.cs
@@ -0,0 +1,23 @@
+﻿namespace Lab1.OptimizationContexts;
+
+public class GoldenRatioOptimisationContext : IOptimizationContext
+{
+    public GoldenRatioOptimisationContext(double a, double b)
+    {
+        A = a;
+        B = b;
+    }
+
+    public GoldenRatioOptimisationContext(double a, double b, double? x1, double? x2)
+    {
+        A = a;
+        B = b;
+        X1 = x1;
+        X2 = x2;
+    }
+
+    public double A { get; }
+    public double B { get; }
+    public double? X1 { get; } = null;
+    public double? X2 { get; } = null;
+}
