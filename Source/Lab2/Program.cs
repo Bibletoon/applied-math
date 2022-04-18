1239fb960d4975ad9db1f2f957ba339e675f1c38
Bibletoon <alex8.ivanov@gmail.com>
2022-04-18 16:55:59 +0300

    Feat: add contour series generation

--- Source/Lab2/Program.cs
+++ Source/Lab2/Program.cs
@@ -3,12 +3,25 @@
 using Lab2;
 using Lab2.GradientDescent;
 using Lab2.Models;
+using Lab2.Tools;
 using MathNet.Numerics.LinearAlgebra;
 using MathNet.Numerics.LinearAlgebra.Double;
+using OxyPlot;
 
-var function = (Vector<double> point) => 10 * point[0] * point[0] + point[1] * point[1];
+var function = (Vector<double> point) => 2 * point[0] * point[0] + point[0] * point[1] + point[1] * point[1];
 
-var method = new ShrinkStepGradientDescentMethod(1, 0.1, 0.95);
-var startPoint = new DenseVector(new []{10.0, 10.0});
-var result = method.FindMinimum(new OptimizationTask(startPoint, 1e-5/2, 1e-5/2, function));
-Console.WriteLine();
+var startPoint = new DenseVector(new []{0.5, 1.0});
+var task = new OptimizationTask(startPoint, 1e-3, 1e-3, function);
+
+var cnst = new ConstStepGradientDescentMethod(1);
+var shrink = new ShrinkStepGradientDescentMethod(1, 0.1, 0.95);
+
+var resultConst = cnst.FindMinimum(task);
+var resultShrink = shrink.FindMinimum(task);
+
+var model = GraphicGenerator.GenerateContourSeries(function, resultConst.PointsHistory.ToList());
+
+using (var stream = File.Create("picture.svg"))
+{
+    SvgExporter.Export(model ,stream, 600, 600, false);
+}
---
+++ Source/Lab2/Tools/GraphicGenerator.cs
@@ -0,0 +1,55 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+using OxyPlot;
+using OxyPlot.Series;
+
+namespace Lab2.Tools;
+
+public class GraphicGenerator
+{
+    public static PlotModel GenerateContourSeries(Func<Vector<double>, double> function, List<Vector<double>> points)
+    {
+        var model = new PlotModel() { Title = "ContourSeries" };
+
+        double x0 = points.Select(p => p[0]).Min();
+        double x1 = points.Select(p => p[0]).Max();
+        
+        double y0 = points.Select(p => p[1]).Min();
+        double y1 = points.Select(p => p[1]).Max();
+
+        x0 = x0 * (Math.Sign(x0) == -1 ? 1.2 : 0.8);
+        y0 = y0 * (Math.Sign(y0) == -1 ? 1.2 : 0.8);
+
+        x1 = x1 * (Math.Sign(x1) == -1 ? 0.8 : 1.2);
+        y1 = y1 * (Math.Sign(y1) == -1 ? 0.8 : 1.2);
+
+        Func<double, double, double> peaks = (x, y) => function.Invoke(new DenseVector(new []{x, y}));
+
+        var xx = ArrayBuilder.CreateVector(x0, x1, 500);
+        var yy = ArrayBuilder.CreateVector(y0, y1, 500);
+        var peaksData = ArrayBuilder.Evaluate(peaks, xx, yy);
+
+        var cs = new ContourSeries()
+        {
+            Color = OxyColors.Black,
+            LabelBackground = OxyColors.White,
+            ColumnCoordinates = xx,
+            RowCoordinates = yy,
+            Data = peaksData
+        };
+
+        var line = new LineSeries()
+        {
+            LineStyle = LineStyle.Dash,
+            StrokeThickness = 1,
+            MarkerSize = 2,
+            MarkerType = MarkerType.Circle
+        };
+
+        points.ForEach(p => line.Points.Add(new DataPoint(p[0], p[1])));
+        
+        model.Series.Add(cs);
+        model.Series.Add(line);
+        return model;
+    }
+}
