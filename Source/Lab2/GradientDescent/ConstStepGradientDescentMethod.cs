4219e7a8fc2b677af18676a425c76d13cdc4fc3a
Bibletoon <alex8.ivanov@gmail.com>
2022-04-18 09:47:52 +0300

    Feat: add const step gradient descent method

---
+++ Source/Lab2/GradientDescent/ConstStepGradientDescentMethod.cs
@@ -0,0 +1,31 @@
+﻿using Lab2.Models;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace Lab2.GradientDescent;
+
+public class ConstStepGradientDescentMethod : GradientDescentMethod
+{
+    private double _stepValue;
+
+    public ConstStepGradientDescentMethod(double startStepValue)
+    {
+        _stepValue = startStepValue;
+    }
+
+    protected override Vector<double> GetNextPoint(NextPointFindParameters parameters)
+    {
+        var currentValue = parameters.Function.Invoke(parameters.Point);
+        while (true)
+        {
+            var newPoint = parameters.Point - _stepValue * parameters.Gradient;
+            var functionValue = parameters.Function.Invoke(newPoint);
+
+            if (currentValue - functionValue > 0)
+            {
+                return newPoint;
+            }
+
+            _stepValue /= 2;
+        }
+    }
+}
---
+++ Source/Lab2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Lab2;
+using Lab2.GradientDescent;
+using Lab2.Models;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+var function = (Vector<double> point) => 2 * point[0] * point[0] + point[0] * point[1] + point[1] * point[1];
+
+var method = new ConstStepGradientDescentMethod(0.5);
+var startPoint = new DenseVector(new []{0.5, 1});
+var result = method.FindMinimum(new OptimizationTask(startPoint, 0.1, 0.15, function));
+Console.WriteLine();
