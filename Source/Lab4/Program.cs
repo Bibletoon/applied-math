65e40261a022606994d2f77a7cc100227726bde1
bibletoon <alex8.ivanov@gmail.com>
2022-05-27 19:17:06 +0300

    Feat: method run variants

--- Source/Lab4/Program.cs
+++ Source/Lab4/Program.cs
@@ -8,12 +8,13 @@ public static class Program
 {
     public static void Main(string[] args)
     {
-        var sizes = new[] { 10, 50, 100 };
-        var ks = new[] { 4 };
+        var sizes = new[] { 10, 25, 50 };
+        var ks = new[] { 0, 1, 3, 5, 15 };
         using var reportFile = File.Open("report.md", FileMode.Create);
         using var reportWriter = new StreamWriter(reportFile);
+        
         RunSolution(new SymmetricDiagonallyDominantMatrixGenerator(), sizes, ks, reportWriter);
-        RunSolution(new GilbertMatrixGenerator(), ks, sizes, reportWriter);
+        RunSolution(new GilbertMatrixGenerator(), new []{4}, new []{5, 10, 25, 50, 75, 100, 150, 250, 500}, reportWriter);
     }
 
     private static void RunSolution(ISquareMatrixGenerator<double> generator, int[] sizes, int[] kValues, StreamWriter reportWriter)
@@ -29,25 +30,25 @@ public static class Program
 
                 var result = method.GetEigenvalues(matrix);
                 reportWriter.WriteLine($"## {generator.Name}, size = {size}, k={k}\n");
-                reportWriter.WriteLine(MatrixToString(matrix));
+                // reportWriter.WriteLine(MatrixToString(matrix));
                 reportWriter.WriteLine("\n Found eigen values:\n");
 
-                bool isRightSolution = true;
+                double acc = 0;
                 foreach (var eigenValue in result.EigenValues)
                 {
-                    reportWriter.WriteLine($"Number = {eigenValue.Number}\n");
-                    reportWriter.WriteLine($"Vector = ({string.Join("; ", eigenValue.Vector)})\n");
+                    // reportWriter.WriteLine($"Number = {eigenValue.Number}\n");
+                    // reportWriter.WriteLine($"Vector = ({string.Join("; ", eigenValue.Vector)})\n");
 
-                    isRightSolution = CheckEigenValue(matrix, eigenValue);
+                    acc = Math.Max(CheckEigenValue(matrix, eigenValue), acc);
                 }
 
-                reportWriter.WriteLine($"Is right solution - {isRightSolution}\n");
+                reportWriter.WriteLine($"Is right solution - {acc < 0.1}, accuracy - {acc}\n");
                 reportWriter.WriteLine($"Iterations count - {result.IterationsCount}\n");
             }
         }
     }
 
-    private static bool CheckEigenValue(Matrix<double> matrix, EigenValue eigenValue)
+    private static double CheckEigenValue(Matrix<double> matrix, EigenValue eigenValue)
     {
         var expected = eigenValue.Vector.Multiply(eigenValue.Number);
         var actual = matrix.Multiply(eigenValue.Vector);
@@ -55,17 +56,11 @@ public static class Program
         return CheckVectorsEquality(expected, actual);
     }
 
-    private static bool CheckVectorsEquality(Vector<double> expected, Vector<double> actual)
+    private static double CheckVectorsEquality(Vector<double> expected, Vector<double> actual)
     {
-        for (int i = 0; i < expected.Count; i++)
-        {
-            if (Math.Abs(expected[i] - actual[i]) >= 0.1)
-            {
-                return false;
-            }
-        }
+        double acc = expected.Select((t, i) => Math.Abs(t - actual[i])).Prepend(0).Max();
 
-        return true;
+        return acc;
     }
 
     private static string MatrixToString(Matrix<double> matrix)
